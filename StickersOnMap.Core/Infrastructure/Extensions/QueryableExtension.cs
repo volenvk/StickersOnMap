d769248dbed774dac24dcabe4cc874e43449aa61
vmkovalkovskiy <vmkovalkovskiy@vodokanalekb.ru>
2021-04-08 11:08:47 +0500

    Исправление фильтрации.

--- StickersOnMap.Core/Infrastructure/Extensions/QueryableExtension.cs
+++ StickersOnMap.Core/Infrastructure/Extensions/QueryableExtension.cs
@@ -11,24 +11,58 @@ namespace StickersOnMap.Core.Infrastructure.Extensions
     
     public static class QueryableExtension
     {
-        
         public static IQueryable<T> FilterBy<T>(this IQueryable<T> source, IEnumerable<Filter> filters)
         {
-            return filters.Aggregate(source, (current, filter) => current.FilterBy(filter.Property, filter.Value));
+            return filters.Aggregate(source, (current, filter) =>
+            {
+                var property = filter.Property;
+                return property.Contains("_") 
+                    ? current.FilterByDate(property, filter.Value) 
+                    : current.FilterBy(filter.Property, filter.Value);
+            });
         }
-        
+
         public static IQueryable<T> FilterBy<T>(this IQueryable<T> source, string propertyName, string value) {
             if (string.IsNullOrEmpty(propertyName))
                 throw new ArgumentNullException($"property null or empty on object {typeof(T).Name}");
-            
+
+            var info = GetPropertyInfo<T>(propertyName);
+            return info.PropertyType == typeof(string)
+                ? source.Where($"{info.Name}.Contains(@0)", value)
+                : source.Where($"{info.Name} == (@0)", value);
+        }
+
+        public static IQueryable<T> FilterByDate<T>(this IQueryable<T> source, string propertyName, string value) {
+            if (string.IsNullOrEmpty(propertyName))
+                throw new ArgumentNullException($"property null or empty on object {typeof(T).Name}");
+
+            if (propertyName.Contains("_gte"))
+            {
+                var property = propertyName.Replace("_gte", "");
+                var info = GetPropertyInfo<T>(property);
+                return source.Where($"{info.Name} >= @0", value);
+            }
+
+            if (propertyName.Contains("_lte"))
+            {
+                var property = propertyName.Replace("_lte", "");
+                var info = GetPropertyInfo<T>(property);
+                return source.Where($"{info.Name} <= @0", value);
+            }
+
+            return source.FilterBy(propertyName, value);
+        }
+
+        private static PropertyInfo GetPropertyInfo<T>(string propertyName)
+        {
             var propertyInfo = typeof(T).GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
 
             if (propertyInfo == null)
                 throw new ArgumentException($"property {propertyName} not found on object {typeof(T).Name}");
 
-            return source.Where($"{propertyName}.Contains(@0)", value);
+            return propertyInfo;
         }
-        
+
         public static IQueryable<T> TakePage<T>(this IQueryable<T> src, Page page)
         {
             return page == null ? src : src.TakePage(page.PageNumber, page.PageSize);
@@ -60,7 +94,7 @@ namespace StickersOnMap.Core.Infrastructure.Extensions
 
             var ordering = reverse ? "ASC" : "DESC";
             
-            return src.OrderBy(ordering, propertyName);
+            return src.OrderBy(propertyInfo.Name, ordering);
         }
         
         private static IQueryable<T> TakePage<T>(this IQueryable<T> src, int pageNumber, int pageSize)
